2f2105a110b2d85c520b981ca6e69ca7d1925f75
eolmedog <eolmedogonzalez@gmail.com>
2025-03-12 14:41:58 -0300

    feat: Add success route for Payku subscription

---
+++ app/api/(payku)/subscribe/success/route.js
@@ -0,0 +1,47 @@
+import {NextResponse} from "next/server";
+import {sign} from "../../utils";
+const BASE_URL = process.env.PAYKU_BASE_URL;
+const token_publico = process.env.PAYKU_TOKEN_PUBLICO;
+
+const get_transaction = async (transaction_id) => {
+    const response = await fetch(`${BASE_URL}/api/transaction/${transaction_id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization':  `Bearer ${token_publico}`
+      },
+    });
+    const result = await response.json();
+    return result;
+  }
+
+const get_sub_client = async (client_email) => {
+  const slug = `/api/suclient/${client_email}`
+  const response = await fetch(`https://${BASE_URL}/api/suclient/${client_email}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Sign': sign(slug, client_email),
+      'Authorization': `Bearer ${token_publico}`
+    },
+  });
+  const result = await response.json();
+  console.log(result)
+}
+
+request(data);
+
+
+export async function POST(req) {
+    console.log('POST request received')
+    const transaction_data = await req.json();
+    const transaction_id = transaction_data.wetransaction_id
+    const details = await get_transaction(transaction_id);
+    console.log(details)
+}
+
+export async function GET(req) {
+    console.log('GET request received')
+    const req_data = await req.json();
+    console.log(req_data);
+}
